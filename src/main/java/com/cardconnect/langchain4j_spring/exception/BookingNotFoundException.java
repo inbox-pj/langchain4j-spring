bb2afbdaa48045cd81245b84a6ebf77aff21e120
Pankaj Jaiswal <jaiswal.pankaj10@gmail.com>
2025-12-22 14:50:12 +0530

    Delete src directory

--- src/main/java/com/cardconnect/langchain4j_spring/exception/BookingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cardconnect.langchain4j_spring.exception;
-
-public class BookingNotFoundException extends RuntimeException {
-
-    public BookingNotFoundException(String bookingNumber) {
-        super("Booking " + bookingNumber + " not found");
-    }
-}
--- src/main/java/com/cardconnect/langchain4j_spring/repository/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cardconnect.langchain4j_spring.repository;
-
-import com.cardconnect.langchain4j_spring.entity.ChatMessageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Long> {
-}
